d07e82eb4da7b9d3b9560778e7637662a7e897bb
covy2406 <nguyentranthaovy2406@gmail.com>
2024-11-22 11:38:14 +0700

    [update] bussiness combonent

---
+++ src/components/smallbussiness/constants/data.ts
@@ -0,0 +1,32 @@
+export const listIcons = [
+  {
+    id: 1,
+    icon: "https://theme-land.com/sapp/demo/assets/img/content/icon-1.png",
+    title: "Fully functional",
+    description: "Enjoy a complete suite of features designed for seamless user experiences.",
+  },
+  {
+    id: 2,
+    icon: "https://theme-land.com/sapp/demo/assets/img/content/icon-2.png",
+    title: "Live Chat",
+    description: "Connect with your audience in real-time for instant support and engagement.",
+  },
+  {
+    id: 3,
+    icon: "https://theme-land.com/sapp/demo/assets/img/content/icon-3.png",
+    title: "Secure Data",
+    description: "Protect your information with industry-leading security measures and encryption.",
+  },
+  {
+    id: 4,
+    icon: "https://theme-land.com/sapp/demo/assets/img/content/icon-4.png",
+    title: "Customizable",
+    description: "Customize the app to suit your needs and preferences with ease.",
+  },
+  {
+    id: 5,
+    icon: "https://theme-land.com/sapp/demo/assets/img/content/icon-5.png",
+    title: "Easy to Customize",
+    description: "Protect your information with industry-leading security measures and encryption.",
+  },
+]
